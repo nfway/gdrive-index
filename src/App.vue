794f7a9a007c1861bbc40f556b313183e313916e
nfway <nofway@tuta.io>
2021-04-15 13:23:24 +0800

    1.2

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,7 @@ export default {
   },
   data: function() {
     return {
-      github: "https://github.com/Aicirou/goindex-theme-acrou",
+      github: "https://github.com/nfway/gdrive-index",
     };
   },
   watch: {
--- src/views/common/Footer.vue
+++ src/views/common/Footer.vue
@@ -6,7 +6,7 @@
           <div class="tags has-addons">
             <a
               class="tag is-dark"
-              href="https://github.com/Aicirou/goindex-theme-acrou"
+              href="https://github.com/nfway/gdrive-index"
             >goindex-theme-acrou</a>
             <span class="tag is-light">
               MIT &nbsp;
